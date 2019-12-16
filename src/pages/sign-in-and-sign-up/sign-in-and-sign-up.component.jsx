33a8dcb5040b340134cf40c7db58472395d87dfa
Bas Suckling <suckling.w.s@gmail.com>
2019-12-17 11:38:01 +1300

    setup signin components

---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import './sign-in-and-sign-up.styles.scss'
+import SignIn from '../../components/sign-in/sign-in.component'
+import FormInput from '../../components/form-input/form-input.component'
+
+const SignInAndSignUpPage = () => (
+    <div className="sign-in-and-sign-up">
+        <SignIn/>
+    </div>
+
+)
+
+export default SignInAndSignUpPage

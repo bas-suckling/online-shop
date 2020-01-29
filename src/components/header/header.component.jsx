35cec4d2e27541f44860db4d683ffb0cdc1fe284
Bas Suckling <suckling.w.s@gmail.com>
2020-01-30 10:54:03 +1300

    create shopping cart component and icon

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 import {connect} from 'react-redux'
 import {ReactComponent as Logo} from '../../assets/shaka.svg'
+import CartIcon from '../cart-icon/cart-icon.component'
 
 import './header.styles.scss'
 import { auth } from '../../firebase/firebase.utils'
@@ -17,13 +18,14 @@ const Header = ({currentUser}) => (
         <Link className='option' to='/shop'>CONTACT</Link>
         {
             currentUser ?
-            <div className="option" onClick={() => auth.signOut()}>SIGN OUT</div>
+            (<div className="option" onClick={() => auth.signOut()}>SIGN OUT</div>)
             :
-            <Link className="option" to='/signin'>
+            (<Link className="option" to='/signin'>
                 SIGN IN
-            </Link>
-
+            </Link>)
         }
+        <CartIcon/>
+
     </div>
 </div>
 )
